088ded9d3039ab826073b6281353e3d594ed2326
muhammed.tatar@unipa.com.tr <muhammed.tatar@unipa.com.tr>
2023-09-15 13:48:56 +0300

    Add project files.

---
+++ PIWorksDemo/PlaySongApp.cs
@@ -0,0 +1,78 @@
+﻿using PIWorksDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace PIWorksDemo
+{
+    public class PlaySongApp
+    {
+        public static string GetClientCountByPerPlayCount(List<PlaySongModel> playSongList)
+        {
+            try
+            {
+                var groupedPlaySongList = playSongList.Where(x => x.PLAY_TS == new DateTime(2016, 8, 10))
+                                                  .GroupBy(y => y.CLIENT_ID)
+                                                  .Select(z => new
+                                                  {
+                                                      ClientID = z.Key,
+                                                      PlayCount = z.Select(t => t.SONG_ID).Distinct().Count()
+                                                  })
+                                                  .GroupBy(c => c.PlayCount)
+                                                  .Select(b => new
+                                                  {
+                                                      PLAY_COUNT = b.Key,
+                                                      CLIENT_COUNT = b.Count()
+                                                  });
+
+                var json = JsonSerializer.Serialize(groupedPlaySongList);
+
+                return json;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public static string GetPlayCountPerClienID(List<PlaySongModel> playSongList, int playCount)
+        {
+            try
+            {
+                var playCountList = playSongList.GroupBy(y => y.CLIENT_ID)
+                                                      .Select(z => new
+                                                      {
+                                                          ClientID = z.Key,
+                                                          PlayCount = z.Select(t => t.SONG_ID).Distinct().Count()
+                                                      })
+                                                      .Where(b => b.PlayCount == playCount).Count();
+
+                var json = JsonSerializer.Serialize(playCountList);
+
+                return json;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public static string GetDistinctSongCount(List<PlaySongModel> playSongList)
+        {
+            try
+            {
+                var distinctSongCount = playSongList.Select(t => t.SONG_ID).Distinct().Count();
+
+                return distinctSongCount.ToString();
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+    }
+}
---
+++ PIWorksDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using PIWorksDemo;
+using PIWorksDemo.Models;
+
+List<PlaySongModel> playSongList = new List<PlaySongModel>();
+using (var reader = new StreamReader(@"..\..\..\..\input.csv"))
+{
+    reader.ReadLine();//removing header
+    while (!reader.EndOfStream)
+    {
+        var line = reader.ReadLine();
+        var values = line.Split('\t');
+
+        var playSongModel = new PlaySongModel()
+        {
+            PLAY_ID = values[0],
+            SONG_ID = int.Parse(values[1]),
+            CLIENT_ID = int.Parse(values[2]),
+            PLAY_TS = DateTime.Parse(values[3]),
+        };
+        playSongList.Add(playSongModel);
+    }
+}
+
+var resultQ1 = PlaySongApp.GetClientCountByPerPlayCount(playSongList);
+
+Console.WriteLine(resultQ1);
+
+var resultQ2 = PlaySongApp.GetPlayCountPerClienID(playSongList, 346);
+
+Console.WriteLine(resultQ2);
+
+var resultQ3 = PlaySongApp.GetDistinctSongCount(playSongList);
+
+Console.WriteLine(resultQ3);
+
+//foreach (var playSong in playSongList)
+//{
+//    Console.WriteLine(playSong.PLAY_ID + " " + playSong.CLIENT_ID + " " + playSong.SONG_ID + " " + playSong.PLAY_TS);
+//}
+
+Console.ReadLine();
